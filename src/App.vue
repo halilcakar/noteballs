691b52acc678e4e5b1c43963c59106f6103030b0
halilcakar <hcakar.1992@gmail.com>
2022-05-30 18:42:09 +0300

    feat: :sparkles: Bulma & Design
     - Install Bulma
     - Nav Bar - Design
     - Nav Bar - Navigation & logo
     - Nav Bar - Responsive Design & Menu
     - Pages (Design)
     - Notes (Design)
     - Add Note Form (Design)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Links</RouterLink>
-  <main>
+  <NavBar />
+  <main class="container is-max-desktop px-2 py-4">
     <RouterView />
   </main>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import NavBar from "@/components/Layout/NavBar.vue";
 </script>
+
+<style>
+@import "bulma/css/bulma.min.css";
+</style>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="notes">
-    <h1>notes</h1>
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Notes
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div v-for="i in 3" :key="i" class="card mb-4">
+      <main class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+          voluptate reiciendis, natus deleniti eius quis, esse eum, magni sit
+          minus molestias nemo voluptates dolor? Autem omnis fugiat quaerat!
+          Porro, harum!
+        </div>
+      </main>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
   </div>
 </template>
